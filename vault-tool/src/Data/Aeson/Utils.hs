95e5f0143e56944c8b8acb5d218eacde4bc9b47b
Timothy Miller <timothy.miller@bitnomial.com>
2022-05-13 14:10:54 -0400

    Updated stack lts, updated impacted code

--- vault-tool/src/Data/Aeson/Utils.hs
+++ vault-tool/src/Data/Aeson/Utils.hs
@@ -1,26 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Data.Aeson.Utils (
     object,
     (.=!),
     (.=?),
-    DataWrapper (..)
+    DataWrapper (..),
 ) where
 
-import Data.Aeson (FromJSON, KeyValue, ToJSON, Value, (.=), (.:), withObject)
+import Data.Aeson (FromJSON, Key, KeyValue, ToJSON, Value, withObject, (.:), (.=))
 import qualified Data.Aeson as Aeson
+import Data.Aeson.Types (Pair)
 import Data.Maybe (catMaybes)
-import Data.Text (Text)
 
-object :: [Maybe (Text, Value)] -> Value
+object :: [Maybe Pair] -> Value
 object = Aeson.object . catMaybes
 
-(.=!) :: (KeyValue a, ToJSON b) => Text -> b -> Maybe a
+(.=!) :: (KeyValue a, ToJSON b) => Key -> b -> Maybe a
 k .=! v = Just $ k .= v
 
-(.=?) :: (Functor f, KeyValue a, ToJSON b) => Text -> f b -> f a
+(.=?) :: (Functor f, KeyValue a, ToJSON b) => Key -> f b -> f a
 k .=? v = (k .=) <$> v
 
-newtype DataWrapper a = DataWrapper { unDataWrapper :: a }
+newtype DataWrapper a = DataWrapper {unDataWrapper :: a}
 
 instance ToJSON a => ToJSON (DataWrapper a) where
     toJSON (DataWrapper x) = object ["data" .=! x]
